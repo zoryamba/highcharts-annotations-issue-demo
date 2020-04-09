ff09a95eefd3874aeb6de05f995c0dc58a6c4b82
zoryamba <zoryamba@gmail.com>
2020-04-09 16:26:24 +0300

    add code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+import Annotations from "highcharts/modules/annotations";
+
+Annotations(Highcharts);
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            options: {
+                series: [{
+                    data: [
+                        [0, 1],
+                        [1, 2],
+                        [2, 5],
+                        [3, 2],
+                        [4, 1],
+                        [5, 3],
+                        [6, 4],
+                        [7, 4],
+                        [8, 3],
+                    ]
+                }]
+            },
+        };
+    }
+
+    componentDidMount() {
+
+        const annotations = [{
+            labels: [{
+                point: {
+                    xAxis: 0,
+                    yAxis: 0,
+                    x: 5,
+                    y: 3,
+                },
+                text: "annotation",
+            }]
+        }];
+
+        const yAxis = [{
+            plotLines: [
+                {
+                    width: 2,
+                    label: {
+                        text: "plotline",
+                    },
+                    value: 3
+                }
+            ]
+        }];
+
+
+        this.setState({
+            options: {
+                yAxis: yAxis,
+                annotations: annotations,
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <HighchartsReact
+                    highcharts={Highcharts}
+                    constructorType={"chart"}
+                    options={this.state.options}
+                />
+            </div>
+        );
+    }
 }
+
 
 export default App;
